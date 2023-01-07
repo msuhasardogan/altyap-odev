3604b4a0dd55373cccafbe88649d392fa84a9864
msuhasardogan <mehmetsuha95@gmail.com>
2023-01-07 17:12:01 +0300

    kodlar yazıldı

--- proje.sh
+++ proje.sh
@@ -1,1 +1,16 @@
-deneme
+#!/bin/bash
+
+
+directory="/home/vboxuser/Masaüstü/altyap-odev/proje.sh"
+
+
+for file in $directory/*; do
+
+  filename=$(basename "$file")
+ 
+  deletedate=$(stat -c %y "$file")
+ 
+  createdate=$(stat -c %w "$file")
+ 
+  echo "$createdate $deletedate $filename" >> /home/vboxuser/Masaüstü/altyap-odev/odev.txt
+done
